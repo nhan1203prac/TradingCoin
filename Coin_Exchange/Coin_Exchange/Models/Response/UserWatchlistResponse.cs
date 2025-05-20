891cea2a49493548f659b3be364951dc877aaa32
VinhNguyenUte <valtriek99@gmail.com>
2025-05-20 18:24:39 +0700

    Thêm Watchlist và Withdrawel Controller

---
+++ Coin_Exchange/Coin_Exchange/Models/Response/UserWatchlistResponse.cs
@@ -0,0 +1,15 @@
+﻿using Coin_Exchange.Models.Modal;
+
+namespace Coin_Exchange.Models.Response
+{
+    public class UserWatchlistResponse
+    {
+        public long watchlistId { get; set; }
+
+
+        public long userId { get; set; }
+        public User user { get; set; }
+
+        public List<WatchlistCoin> WatchlistCoins { get; set; } = new List<WatchlistCoin>();
+    }
+}
